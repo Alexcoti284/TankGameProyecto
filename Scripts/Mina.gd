6740ecd36895bb616ffd00157830ddbf4f5cf1f9
Alexcoti284 <118183101+Alexcoti284@users.noreply.github.com>
2025-01-26 15:12:38 +0100

    minas, tipos de bala i animaciones
    Completamos las modificaciones de las balas

---
+++ Scripts/Mina.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var Blast = preload("res://Escenas/Efectos/Explosion.tscn")
+
+var exploding = false
+
+func _ready():
+	AudioManager.play(AudioManager.SOUNDS.MINE)
+
+func setup(position: Vector2):
+	self.position = position
+
+func createBlast():
+	var blast = Blast.instance()
+	blast.position = position
+	get_parent().add_child(blast)
+
+func destroy():
+	call_deferred("createBlast")
+	queue_free()
+
+func _on_ExpireTimer_timeout():
+	$BlastTimer.start()
+	$Tick_Sound.play()
+	$AnimationPlayer.play("tick")
+
+func _on_BlastTimer_timeout():
+	destroy()
